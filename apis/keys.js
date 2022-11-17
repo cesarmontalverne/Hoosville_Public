e945554b0dec5747146cf728af72b19a6f4a6690
cesarmontalverne <ca6fqb@virginia.edu>
2022-11-17 13:58:09 -0500

    App Completed Draft 2

--- apis/keys.js
+++ apis/keys.js
@@ -1,3 +1,5 @@
+//Altered for safety reasons
+
 class keys{
     constructor(sheets_token, sheets_id, firebaseConfig){
         this.sheets_token = sheets_token
@@ -10,16 +12,16 @@ class keys{
 }
 
 const ks = new keys(
-    "AIzaSyAIMQ8rU-hO7e6gJ1FsluQBhDh54diCOlk", //sheets_token
-    "18R94YiYe3035ng9NPtfdk7PJQABlgvCRfOobvQA-IbE", // sheets_id
+    "", //sheets_token
+    "", // sheets_id
     { //firebaseConfig JSON
-    apiKey: "AIzaSyAIj24HKHoZ1m6rXT7QIiyPJFHZpKN3uG4",
-    authDomain: "friendme-3cb2a.firebaseapp.com",
-    projectId: "friendme-3cb2a",
-    storageBucket: "friendme-3cb2a.appspot.com",
-    messagingSenderId: "367045694611",
-    appId: "1:367045694611:web:49bee45cc952c8c599a966",
-    measurementId: "G-KXYNKVHP3S"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   })
 
 export default ks;
